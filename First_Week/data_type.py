c358c739dd5e4eb42186addb9345eff749fce6bc
jalg2142 <110517117+jalg2142@users.noreply.github.com>
2024-04-21 11:52:36 +0530

    python datatypes

---
+++ First_Week/data_type.py
@@ -0,0 +1,23 @@
+x = "python{}"
+y = 'elephant'
+z = """\nHello world....
+    jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+    pppppppppppppppppppppppppppppppppppppppp
+    """
+# print(type(x),x)
+# checked the type.
+print(x,y,z)
+print(x[0])
+
+
+a = 0
+print(type(a))
+print(x.format(a))
+print(x+y+str(a))
+
+
+print(f"janith Algewatta {y}")
+
+b = 0.0
+print(type(b))
+
